93ef9adebaa022ab72a7735e1b41a054228876ad
GZhonghui <mail@gzher.com>
2024-10-24 01:03:45 +0900

    错误处理

---
+++ 07_handle_error/07_handle_error.cu
@@ -0,0 +1,32 @@
+// 只是几个代码片段，通不过编译
+
+// 应该是因为定义在了头文件里，所以用了static
+static void HandleError( cudaError_t err,
+                         const char *file,
+                         int line ) {
+    if (err != cudaSuccess) {
+        printf( "%s in %s at line %d\n", cudaGetErrorString( err ),
+                file, line );
+        exit( EXIT_FAILURE );
+    }
+}
+#define HANDLE_ERROR( err ) (HandleError( err, __FILE__, __LINE__ ))
+ 
+ 
+#define HANDLE_NULL( a ) {if (a == NULL) { \
+                            printf( "Host memory failed in %s at line %d\n", \
+                                    __FILE__, __LINE__ ); \
+                            exit( EXIT_FAILURE );}}
+
+// check cuda error
+inline void check(cudaError_t call, const char* file, const int line)
+{
+    if (call != cudaSuccess)
+    {
+        std::cout << "cuda error: " << cudaGetErrorName(call) << std::endl;
+        std::cout << "at file: " << file << ", line: " << line << std::endl;
+        std::cout << cudaGetErrorString(call) << std::endl;
+    }
+}
+ 
+#define CHECK(call) (check(call, __FILE__, __LINE__))
